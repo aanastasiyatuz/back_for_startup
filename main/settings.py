cb9f82dc0551b4b1a3f8d2c5950711edaa7038ba
aanastasiyatuz <aanastasiyatuz@gmail.com>
2021-05-26 16:15:50 +0600

    django_heroku

--- main/settings.py
+++ main/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from decouple import config
 import dj_database_url
+import django_heroku
 
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
