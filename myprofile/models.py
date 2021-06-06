e79ff74e5e162b90bdeae49ce9aaecd3c03e4aff
aanastasiyatuz <aanastasiyatuz@gmail.com>
2021-06-06 20:29:33 +0600

    pk on slug

--- myprofile/models.py
+++ myprofile/models.py
@@ -7,11 +7,11 @@ MyUser = get_user_model()
 
 class ProfileProvider(models.Model):
     user = models.OneToOneField(MyUser, on_delete=models.CASCADE, related_name='provider')
-    email = models.EmailField(unique=True, primary_key=True)
+    email = models.EmailField(unique=True)
     avatar = models.ImageField(upload_to='providers', default='default-avatar.jpg')
     country = models.CharField(max_length=100, blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
-    slug = models.SlugField(unique=True, blank=True)
+    slug = models.SlugField(unique=True, blank=True, primary_key=True)
 
     def __str__(self):
         return self.email
@@ -22,11 +22,11 @@ class ProfileProvider(models.Model):
 
 class ProfileClient(models.Model):
     user = models.OneToOneField(MyUser, on_delete=models.CASCADE, related_name='client')
-    email = models.EmailField(unique=True, primary_key=True)
+    email = models.EmailField(unique=True)
     avatar = models.ImageField(upload_to='clients', default='default-avatar.jpg')
     country = models.CharField(max_length=100, blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
-    slug = models.SlugField(unique=True, blank=True)
+    slug = models.SlugField(unique=True, blank=True, primary_key=True)
 
     def __str__(self):
         return self.email
